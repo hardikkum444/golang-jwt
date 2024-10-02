73b561c7388d4688251ff4ce355d10ccc2195403
hardikkum444 <hardikkumawat444@gmail.com>
2024-10-03 00:41:22 +0530

    minor corrections

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
     "os"
     "github.com/gin-gonic/gin"
     routes "golang-jwt/routes"
+    "github.com/joho/godotenv"
+    "log"
 )
 
 func main() {
+    
+    err := godotenv.Load(".env")
 
+    if err!=nil {
+        log.Fatal("Error loading the .env file")
+    }
+    
     port := os.Getenv("PORT")
 
     if port==""{
@@ -20,12 +28,12 @@ func main() {
     routes.AuthRoutes(router)
     routes.UserRoutes(router)
 
-    router.GET("/api-1", func(c *gin-Context){
+    router.GET("/api-1", func(c *gin.Context) {
         
         c.JSON(200, gin.H{"success":"Access granted for api-1"})
     })
 
-    router.GET("/api-2", func(c *gin-Context){
+    router.GET("/api-2", func(c *gin.Context) {
 
         c.JSON(200, gin.H{"success":"Access granted for api-2"})
     })
