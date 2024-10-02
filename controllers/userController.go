3f7d289e7cf7765f79593158b7da78fdd10226f2
hardikkum444 <hardikkumawat444@gmail.com>
2024-10-02 18:52:05 +0530

    adding more into the signup helpers

--- controllers/userController.go
+++ controllers/userController.go
@@ -14,6 +14,7 @@ helper "golang-jwt/helpers"
 "golang-jwt/models"
 "golang-jwt/helpers"
 "golang.org/x/crypto/bcrypt"
+"go.mongodb.org/mongo-driver/bson"
 
 )
 
@@ -24,13 +25,50 @@ func HashPassword()
 
 func VerifyPassword()
 
-func Signup()
+func Signup() gin.HandlerFunc {
+    
+    return func(c *gin.Context){
+        
+        var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+        var user models.User
+
+        if err := c.BindJSON(&user); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+
+        validationError := validate.Struct(user)
+        if validationError != nil{
+            c.JSON(http.StatusBadRequest, gin.H{"error": validationError.Error()})
+            return
+        }
+
+        count, err := userCollection.CountDocuments(ctx, bson.M{"email":user.Email})
+        defer cancel()
+        if err != nil {
+            log.Panic(err)
+            c.JSON(http.StatusInternalServerError, gin.H{"error":"error occurred while checking for email validity"})
+        }
+
+        count, err = userCollection.CountDocuments(ctx, bson.M{"phone":user.Phone})
+        defer cancel()
+        if err != nil {
+            log.Panic(err)
+            c.JSON(http.StatusInternalServerError, gin.H{"error":"error occurred while checking for phone number validity"})
+        }
+
+        if count > 0{
+            c.JSON(http.StatusInternalServerError, gin.H{"error":"email or phone number already exists"})
+        }
+    }
+}
 
 func Login()
 
 func getUsers()
 
 func getUser() gin.HandlerFunc {
+    
     return func(c *gin.Context){
         
         userId := c.Param("user_id")
